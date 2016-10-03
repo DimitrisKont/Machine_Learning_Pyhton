f61726e03963e1a431cb1865d7297069a34ebe6c
Dimitrios Kontogiannidis <dimitrios.kontogiannidis@nokia.com>
2016-10-03 11:50:40 +0300

    Fixed bug

--- viz.py
+++ viz.py
@@ -3,7 +3,10 @@ ML Python Tutorial
 '''
 import numpy as np
 from sklearn.datasets import load_iris
+from sklearn.externals.six import StringIO
 from sklearn import tree
+from IPython.display import Image
+import pydot
 
 iris = load_iris()
 test_idx = [0, 50, 100]
@@ -21,3 +24,12 @@ clf.fit(train_data, train_target)
 
 print test_target
 print clf.predict(test_data)
+
+dot_data = StringIO()
+tree.export_graphviz(clf, out_file=dot_data,
+                    feature_names=iris.feature_names,
+                    class_names=iris.target_names,
+                    filled=True, rounded=True,
+                    impurity=False)
+graph = pydot.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf("iris.pdf")
