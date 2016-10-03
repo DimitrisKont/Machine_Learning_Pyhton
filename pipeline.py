7a7d8328a8333ad5901f8e20a4f224d0ecfb6486
Dimitrios Kontogiannidis <dimitrios.kontogiannidis@nokia.com>
2016-10-03 13:30:23 +0300

    v0.4.2 accuracy added

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,8 @@
 from sklearn import datasets
 from sklearn import tree
 from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+
 
 iris = datasets.load_iris()
 
@@ -9,13 +11,14 @@ X = iris.data
 # Labels
 y = iris.target
 
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5)
-
 
 my_classifier = tree.DecisionTreeClassifier()
 
 my_classifier.fit(X_train, y_train)
 
 predictions = my_classifier.predict(X_test)
+
 print predictions
+
+print accuracy_score(y_test, predictions)
